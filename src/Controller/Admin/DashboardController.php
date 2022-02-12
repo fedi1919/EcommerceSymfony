e846eec2e8dc05711a08ea6a83dd7219b09e612e
fedi1919 <fedi.hammami.stic@gmail.com>
2022-02-12 23:36:13 +0100

    L'administration

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    public function __construct(
+        private AdminUrlGenerator $adminUrlGenrator
+        ){
+
+        }
+
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+       
+
+        $url = $this->adminUrlGenrator
+        ->setController(UserCrudController::class)
+        ->generateUrl();
+        
+        return $this->redirect($url);
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('E Commerce');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Utilisateurs', 'fa fa-user', User::class);
+    }
+}
